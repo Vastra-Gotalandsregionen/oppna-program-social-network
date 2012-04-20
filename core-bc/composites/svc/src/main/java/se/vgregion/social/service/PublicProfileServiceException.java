91d83da9f33b34ba20b6958e4eb072a70226b220
patrik.bergstrom <patrik.bergstrom@knowit.se>
2012-04-20 06:57:19 +0000

    first commit

---
+++ core-bc/composites/svc/src/main/java/se/vgregion/social/service/PublicProfileServiceException.java
@@ -0,0 +1,11 @@
+package se.vgregion.social.service;
+
+/**
+ * @author Patrik Bergström
+ */
+public class PublicProfileServiceException extends Throwable {
+
+    public PublicProfileServiceException(Throwable cause) {
+        super(cause);
+    }
+}
